ebbcd76419224ca6eb46e3cb47154985a0ded949
Deivisson Santos <83034488+odeivissonsantos@users.noreply.github.com>
2021-05-15 23:19:44 -0300

    Create Controlador.java

---
+++ Controlador.java
@@ -0,0 +1,8 @@
+
+public interface Controlador {
+	public void mostrarLivro();
+	public void cadastrarLivro();
+	public void pesquisarLivro();
+	public void excluirLivro();
+	public void listarLivro();
+}
