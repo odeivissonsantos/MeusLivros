311a482e5a1391f7f79242ec63bbad76b4fa199b
Deivisson Santos <deivissonsantos@hotmail.com>
2021-11-16 17:55:53 -0300

    implementado a configuracao de acesso ao banco de dados h

---
+++ back-end/src/main/java/io/odeivissonsantos/mybooksapp/models/Usuario.java
@@ -0,0 +1,41 @@
+package io.odeivissonsantos.mybooksapp.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    @NotNull(message = "Campo NOME é obrigatório!")
+    private String nome;
+
+    @Column
+    @NotNull(message = "Campo SOBRENOME é obrigatório!")
+    private String sobrenome;
+
+    @Column
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataCadastro;
+
+    @Column
+    @NotNull(message = "Campo EMAIL é obrigatório!")
+    @Email(message = "Campo EMAIL está inválido!")
+    private String email;
+
+}
